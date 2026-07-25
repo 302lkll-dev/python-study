18879d3d8a7bcf2b9083ff9f3ab7572e448adf72
302lkll-dev <302lkll@gmail.com>
2026-07-25 21:45:59 +0900

    review

--- day2.py
+++ day2.py
@@ -1,5 +1,2 @@
-name = input("이름: ")
-age = input("나이: ")
-city = input("사는 곳: ")
-
-print(f"저는 {name}이고, {age}살이며, {city}에 삽니다.")
+s=" 2026. 07. 25"
+print(s.strip().split("."))
---
+++ name=min.py
@@ -0,0 +1,3 @@
+a=input("3: ")
+b=input("4: ")
+print(a + b)
